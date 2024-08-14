1349a0c0acf6c2fa87ab9eaeb70ed1cc4657fcb7
Takunda Madechangu <takundam@sonity.net>
2024-08-14 17:17:49 -0400

    Add sections that link to docs

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,56 +1,75 @@
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import Link from '@docusaurus/Link';
 
 type FeatureItem = {
   title: string;
+  link: string;
   Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Campaigns',
+    link: '/docs/category/campaigns',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        A campaign is a predefined set of actions designed to achieve a specific goal. These actions
+        are executed against contacts.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Integration',
+    link: '/docs/category/integration',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Sonity allows you to integrate with different platforms and tools. This guide 
+        teaches you how to accomplish that.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Account Management',
+    link: '/docs/category/managing-your-account',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Besides managing campaigns, knowing how to manage your account(s) can go a long
+        way.  This section will guide you through that...      
+      </>
+    ),
+  },
+  {
+    title: 'Using Sonity',
+    link: '/docs/category/using-sonity',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        Of course whilst using Sonity you will have many questions or encounter some roadblocks. 
+        Questions  may revolve around automation, campaigns,  contacts....
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({title, Svg, description, link}: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    
+      <div className={clsx('col col--4')}>
+        
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        {/* <Svg className={styles.featureSvg} role="img" /> */}
       </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
+      <div className="padding-horiz--md">
+      <Link href={link}><Heading as="h3">{title}</Heading> </Link>
         <p>{description}</p>
       </div>
+     
     </div>
   );
 }
